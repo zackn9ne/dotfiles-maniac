a5499be5ea939d49407f808656306c3dd5e8c13d
zackn9ne <zackn9ne@gmail.com>
2018-08-12 17:08:55 -0400

    Update README.md

--- emacs_modularized/ivy_swiper_counsel.el
+++ emacs_modularized/ivy_swiper_counsel.el
@@ -18,7 +18,7 @@
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
 
 
-(global-set-key (kbd "C-x C-r") 'counsel-ag)
+(global-set-key (kbd "C-x r") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
