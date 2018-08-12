a6db85b6a499f3afb3e4d856f809c2251f1be2e6
zackn9ne <zackn9ne@gmail.com>
2018-08-12 16:11:31 -0400

    fixed with magit and projectile switch project

--- emacs_modularized/ivy_swiper_counsel.el
+++ emacs_modularized/ivy_swiper_counsel.el
@@ -24,9 +24,3 @@
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 
-;; now with projectile ability
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-
-(setq projectile-switch-project-action 'projectile-vc)
---
+++ emacs_modularized/projectile.el
@@ -0,0 +1,13 @@
+;; now with projectile ability
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; keybindings
+(global-set-key (kbd "C-x p")  'projectile-find-file)
+(global-set-key (kbd "C-x P")  'projectile-switch-project)
+
+;; interact with magit to set default repo to the one you're in
+(setq projectile-switch-project-action 'projectile-vc)
+
+
