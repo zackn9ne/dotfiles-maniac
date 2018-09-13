295ac4ab8bee2852414994200ac5259c7fc03f5c
zackn9ne <zackn9ne@gmail.com>
2018-09-13 15:47:14 +0000

    blah

--- emacs_modularized/theme.el
+++ emacs_modularized/theme.el
@@ -6,5 +6,10 @@
 ;(load-theme 'tango-dark t) /*part of defaults
 ;(load-theme 'monokai t)
 ;(load-theme 'calmer-forest t)
+(load-theme 'retro-green t)
 (set-frame-font "Terminus-14")
 (tool-bar-mode 0)
+
+
+(use-package color-theme-modern
+  :ensure t)
